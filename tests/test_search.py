bcff919a651b73d34f78d3a9e0adbbfb765701c5
Andrew Udvare <audvare@gmail.com>
2023-09-21 15:22:28 -0400

    tests/test_search: add more tests

--- tests/test_search.py
+++ tests/test_search.py
@@ -5,34 +5,9 @@ import requests_mock as req_mock
 
 from choco.main import main as choco
 
-FEED_HEADER = '''<?xml version="1.0" encoding="utf-8" standalone="yes"?>
-<feed xml:base="http://community.chocolatey.org/api/v2/"
-    xmlns:d="http://schemas.microsoft.com/ado/2007/08/dataservices"
-    xmlns:m="http://schemas.microsoft.com/ado/2007/08/dataservices/metadata"
-    xmlns="http://www.w3.org/2005/Atom">
-  <title type="text">Packages</title>
-  <id>http://community.chocolatey.org/api/v2/Packages</id>
-  <updated>2023-09-20T15:12:14Z</updated>
-  <link rel="self" title="Packages" href="Packages" />'''
-FEED_FOOTER = '</feed>'
-BAD_ENTRY_OUTPUT = '''no title? no version?
- Title: no title? | Published: n/a
- Package approved as a trusted package on n/a.
- Package testing status: n/a on n/a.
- Number of Downloads: n/a | Downloads for this version: n/a
- Package url n/a
- Chocolatey Package Source: n/a
- Tags: n/a
- Software Site: n/a
- Software License: n/a
- Documentation: n/a
- Summary: n/a
- Description: n/a
- Release Notes: n/a'''
 
-
-def test_search_no_options_bad_entry(runner: CliRunner, requests_mock: req_mock.Mocker) -> None:
-    id_field = "<id>http://somehost/Packages(Id='somename',Version='2.0.0')</id>"
+def test_search_no_options_bad_entry(runner: CliRunner, requests_mock: req_mock.Mocker,
+                                     bad_feed: str) -> None:
     terms = 'rt-hd-dump'
     qs = urlencode(
         {
@@ -45,13 +20,57 @@ def test_search_no_options_bad_entry(runner: CliRunner, requests_mock: req_mock.
             '$top': '30'
         },
         quote_via=quote)
-    requests_mock.get(f'https://somehost/Packages()?{qs}',
-                      text=f'''{FEED_HEADER}<entry>
-    {id_field}
-</entry>{FEED_FOOTER}''')
-    run = runner.invoke(choco, ('search', '-s', 'https://somehost', '-d', terms))
-    for line in BAD_ENTRY_OUTPUT.splitlines():
-        assert line in run.stdout
-    assert FEED_HEADER in run.stdout
-    assert id_field in run.stdout
+    requests_mock.get(f'https://somehost/Packages()?{qs}', text=bad_feed)
+    run = runner.invoke(choco, ('search', '-s', 'https://somehost', terms))
+    assert run.stdout == ''
     assert run.exit_code == 0
+
+
+def test_search_no_options_bad_entry_id_only(runner: CliRunner, requests_mock: req_mock.Mocker,
+                                             bad_feed: str) -> None:
+    terms = 'rt-hd-dump'
+    qs = urlencode(
+        {
+            '$filter': f"((((Id ne null) and substringof('{terms}',tolower(Id))) or "
+                       f"((Description ne null) and substringof('{terms}',tolower(Description))))"
+                       f" or ((Tags ne null) and substringof(' {terms} ',tolower(Tags)))) "
+                       "and IsLatestVersion",
+            '$orderby': 'Id',
+            '$skip': '0',
+            '$top': '30'
+        },
+        quote_via=quote)
+    requests_mock.get(f'https://somehost/Packages()?{qs}', text=bad_feed)
+    for run in (runner.invoke(choco, ('search', '--id-only', '-s', 'https://somehost', terms)),
+                runner.invoke(choco, ('search', '--idonly', '-s', 'https://somehost', terms))):
+        assert run.stdout == ''
+        assert run.exit_code == 0
+
+
+def test_search_all_versions_exact(runner: CliRunner, requests_mock: req_mock.Mocker,
+                                   chrome_all_versions_exact_feed: str) -> None:
+    terms = 'chrome'
+    qs = urlencode({'id': f"'{terms}'"}, quote_via=quote)
+    requests_mock.get(f'https://somehost/FindPackagesById()?{qs}',
+                      text=chrome_all_versions_exact_feed)
+    for run in (runner.invoke(choco, ('search', '-a', '-e', '-s', 'https://somehost', terms)),
+                runner.invoke(choco,
+                              ('search', '--all', '--exact', '-s', 'https://somehost', terms)),
+                runner.invoke(choco,
+                              ('search', '--allversions', '-e', '-s', 'https://somehost', terms)),
+                runner.invoke(choco,
+                              ('search', '--all-versions', '-e', '-s', 'https://somehost', terms))):
+        lines = run.stdout.splitlines()
+        assert lines[0].strip() == 'GoogleChrome 111.0.5563.111 [Approved]'
+        assert lines[-1].strip() == 'GoogleChrome 10.0.0 [Approved]'
+        assert run.exit_code == 0
+
+
+def test_search_no_duplicates(runner: CliRunner, requests_mock: req_mock.Mocker,
+                              dupe_feed: str) -> None:
+    terms = 'chrome'
+    qs = urlencode({'id': f"'{terms}'"}, quote_via=quote)
+    requests_mock.get(f'https://somehost/FindPackagesById()?{qs}', text=dupe_feed)
+    run = runner.invoke(choco, ('search', '-a', '-e', '-s', 'https://somehost', terms))
+    lines = run.stdout.splitlines()
+    assert len(lines) == 1
