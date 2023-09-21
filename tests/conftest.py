b3ca5a2dcafe2c89c952b951e21c4ff6294d6107
Andrew Udvare <audvare@gmail.com>
2023-09-21 15:22:23 -0400

    tests/conftest: add fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 """Configuration for Pytest."""
+from os.path import dirname
 from typing import NoReturn
 import os
 
@@ -18,5 +19,46 @@ if os.getenv('_PYTEST_RAISE', '0') != '0':  # pragma no cover
 
 
 @pytest.fixture()
+def conftest_dirname() -> str:
+    return dirname(__file__)
+
+
+@pytest.fixture()
 def runner() -> CliRunner:
     return CliRunner()
+
+
+@pytest.fixture()
+def bad_feed(conftest_dirname: str) -> str:
+    with open(f'{conftest_dirname}/feeds/bad.xml') as f:
+        return f.read()
+
+
+@pytest.fixture()
+def firefox_feed(conftest_dirname: str) -> str:
+    with open(f'{conftest_dirname}/feeds/firefox.xml') as f:
+        return f.read()
+
+
+@pytest.fixture()
+def firefox_feed_exact(conftest_dirname: str) -> str:
+    with open(f'{conftest_dirname}/feeds/firefox-exact.xml') as f:
+        return f.read()
+
+
+@pytest.fixture()
+def chrome_all_versions_feed(conftest_dirname: str) -> str:
+    with open(f'{conftest_dirname}/feeds/chrome-all-versions.xml') as f:
+        return f.read()
+
+
+@pytest.fixture()
+def chrome_all_versions_exact_feed(conftest_dirname: str) -> str:
+    with open(f'{conftest_dirname}/feeds/chrome-all-versions-exact.xml') as f:
+        return f.read()
+
+
+@pytest.fixture()
+def dupe_feed(conftest_dirname: str) -> str:
+    with open(f'{conftest_dirname}/feeds/dupe.xml') as f:
+        return f.read()
