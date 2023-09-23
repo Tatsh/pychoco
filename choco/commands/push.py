d0a48fb47fe036c857ebd3e78b25fe710585ab0f
Andrew Udvare <audvare@gmail.com>
2023-09-23 17:56:28 -0400

    choco/commands/push: pass paths to read_all

--- choco/commands/push.py
+++ choco/commands/push.py
@@ -12,21 +12,21 @@ from ..constants import PYCHOCO_API_KEYS_TOML_PATH, PYCHOCO_TOML_PATH
               default=PYCHOCO_TOML_PATH,
               help='Configuration storage file.',
               metavar='PATH',
-              type=click.Path(dir_okay=False, resolve_path=True))
+              type=click.Path(dir_okay=False, resolve_path=True, readable=True))
 @click.option('--api-keys-path',
               default=PYCHOCO_API_KEYS_TOML_PATH,
               help='API keys storage file.',
               metavar='PATH',
-              type=click.Path(dir_okay=False, resolve_path=True))
+              type=click.Path(dir_okay=False, resolve_path=True, readable=True))
 @click.argument('package', type=click.Path(dir_okay=False, readable=True))
 def push(package: str,
          source: str | None = None,
          config_path: str | None = None,
          api_keys_path: str | None = None) -> None:
     """Push a package to a source."""
-    config, api_keys = read_all()
+    config, api_keys = read_all(config_path, api_keys_path)
     try:
         ChocolateyClient(config, api_keys).push(package, source)
     except HTTPError as e:
         click.echo(e.response)
-        raise click.Abort()
+        raise click.Abort() from e
